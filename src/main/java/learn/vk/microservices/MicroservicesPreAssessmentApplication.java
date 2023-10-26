7333849d0b376b720359892bb177d1c1d01ba7c0
Vamsi <vamsikrishna.bhagi@aig.com>
2023-10-26 12:22:08 -0400

    Initial Commit

---
+++ src/main/java/learn/vk/microservices/MicroservicesPreAssessmentApplication.java
@@ -0,0 +1,13 @@
+package learn.vk.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPreAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPreAssessmentApplication.class, args);
+	}
+
+}
