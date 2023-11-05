ef3c3d308c0553746a504fe56d27f15511c45089
Vamsi <vamsikrishna.bhagi@aig.com>
2023-11-05 15:23:15 -0500

    Monolith

---
+++ monolith/src/main/java/learn/vk/microservices/MonolithApplication.java
@@ -0,0 +1,13 @@
+package learn.vk.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithApplication.class, args);
+	}
+
+}
