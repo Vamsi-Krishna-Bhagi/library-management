57f28417199d0835d72052756a12aa9bea428b14
Vamsi <vamsikrishna.bhagi@aig.com>
2023-11-05 15:22:39 -0500

    Monolith

---
+++ monolith/src/main/java/learn/vk/microservices/configuration/SpringFoxConfig.java
@@ -0,0 +1,19 @@
+package learn.vk.microservices.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build();
+    }
+}
